294b86eaeb7ddb8306112489d6b763ab392a337b
Carlos Jorge Gonzalez Perales <carlosgzzperales@gmail.com>
2024-05-06 19:54:41 -0600

    fetched random cat image

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,7 @@
 const buttonEl = document.getElementById ('click1');
 const buttonEl2 = document.getElementById ('click2');
 const textStory = document.getElementById ('changeText');
+const imageContainer = document.getElementById ('kitten');
 
 function changeContent() {
     textStory.innerHTML = '<div> hello, I changed when you clicked the button </div>';
@@ -9,4 +10,18 @@ function changeContent() {
 buttonEl.addEventListener('click', changeContent);
 buttonEl2.addEventListener('click', changeContent);
 
+
+  fetch("http://shibe.online/api/cats?count=[1-100]", 
+    {
+        cache: 'reload'
+    })
+    .then(function (response)
+    {
+        return response.json();
+    })
+    .then(function (data)
+    {
+        document.getElementById('kitten').src = data;
+        console.log(data)
+    });
 
